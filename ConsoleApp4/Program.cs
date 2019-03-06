9dca33238a61c4a51cd5db0429fbe47e8e57e857
visualstudio <visualstudio@122-PC40>
2019-03-06 10:37:09 -0500

    lab 14

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace DelegatesAndEvents
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
+    public delegate int MyDelegate(out int i);
+
+    public class DelegateExercises
+    {
+        int Method1(out int i)
+        {
+            i = 100;
+            System.Console.WriteLine("Method1 " + i);
+            return 0;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = null;
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
+        }
+    }
+
 }
